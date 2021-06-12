59c3b43c62d7aafd9d1bbcd9210f13c2e2de75b4
akisblack <mavrommatisakis@gmail.com>
2021-06-12 17:40:06 +0300

    License fix

---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+import time
+import colorama
+from colorama import Fore
+from colorama import Style
+from subprocess import call
+
+
+def main():
+	# open file in write mode
+	fo = open("names.txt","a")
+	# Input the user data
+	data = input("Whats ur name and telephone?: ")
+	# write data to the file
+	fo.write(os.linesep)
+	fo.write(data)
+	# close the file
+	fo.close()
+
+def find_contact():
+	fh = open("names.txt", "r")
+	word = input("Input the contact you wish to find!\n >")
+	s = " "
+	count = 1
+	
+	while(s):
+		s = fh.readline()
+		L = s.split()
+		if word in L:
+			print("Line Number:", count, ":", s)
+		
+		count += 1
+		
+	
+
+
+
+
+
+def delete_contact():
+				# open file in read mode
+					with open("names.txt", "r") as f:
+						
+						# read data line by line 
+						dataa = f.readlines()
+						
+					# open file in write mode
+					with open("names.txt", "w") as f:
+						
+						for line in dataa :
+							
+							# condition for data to be deleted
+							if line.strip("\n") != delete_input : 
+								f.write(line)
+					
+
+
+def if_four():
+	
+	fo = open("names.txt", 'r')
+	ContactList=fo.read()
+	print(ContactList)
+	print('\n')
+
+
+
+
+print("                              SUPER TELEPHONE BOOK")
+time.sleep(2)
+
+print('''   
+                            ░██████╗████████╗██████╗░
+                            ██╔════╝╚══██╔══╝██╔══██╗
+                            ╚█████╗░░░░██║░░░██████╦╝
+                            ░╚═══██╗░░░██║░░░██╔══██╗
+                            ██████╔╝░░░██║░░░██████╦╝
+                            ╚═════╝░░░░╚═╝░░░╚═════╝░                            
+ ''')
+
+time.sleep(2)
+print("Welcome To The Super Telephone Book\n What do you want to do next?")
+
+
+
+def menu():
+	colorama.init()
+	print(Fore.MAGENTA + Style.DIM + "(01)" + Style.RESET_ALL + "Add new contact")
+	print(Fore.MAGENTA + Style.DIM + "(02)" + Style.RESET_ALL + "Search for contact")
+	print(Fore.MAGENTA + Style.DIM + "(03)" + Style.RESET_ALL + "Delete contact")
+	print(Fore.MAGENTA + Style.DIM + "(04)" + Style.RESET_ALL + "Contact List")
+	print(Fore.MAGENTA + Style.DIM + "(99)" + Style.RESET_ALL + "Exit")
+	
+menu()	
+while True:
+	answer = int(input("Enter your option:"))
+	if answer == 1:
+		main()			
+	elif answer == 2:
+			find_contact()
+	elif answer == 3:
+			if_four()
+			delete_input = input("Input the contact from the ones above that you want to be deleted!\n Note. Copy and paste exactly the contact otherwise it wont work :D\n >")
+			delete_contact()	
+	elif answer == 4:			
+			if_four()	
+	else:
+		print("Input a number given in the options")
+		menu()
+	if answer == 99:
+		quit()		
