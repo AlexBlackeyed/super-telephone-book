2453924b70616a77b1edb1d23f99b493674bfd0a
akisblack <mavrommatisalexander@gmail.com>
2021-08-07 21:27:12 +0300

    new option and other improvements such as cleaner code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from os import name, system
+from genericpath import exists
+from os import name, system, remove, path
 import time
 import colorama
 from colorama import Fore
@@ -6,7 +7,7 @@ from colorama import Style
 import shutil
 import requests
 from datetime import datetime
-
+import subprocess
 
 def centered(s):
     print(s.center(shutil.get_terminal_size().columns))
@@ -101,14 +102,14 @@ def option_one():
 	location = location.capitalize()
 	address = address.capitalize()
 	txt_name = first_name + "_" + surname
-	data = "First Name: " + first_name, "Surname: " +surname, "Telephone Number: " + telephone_number, "Location: " + location, "Address: " +address, "Zip Code: " + zip_code, "Email Address" +email_address, "Birthday" + birthday
+	data = "First Name: " + first_name, "Surname: " +surname, "Telephone Number: " + telephone_number, "Location: " + location, "Address: " +address, "Zip Code: " + zip_code, "Email Address: " +email_address, "Birthday: " + birthday
 	data = list(data)
-	basic_info = first_name + " " + surname
-	with open(''+txt_name+'.txt', "w+") as sh:
+	basic_info = first_name + "_" + surname
+	with open('super-telephone-book\\'+txt_name+'.txt', "w+") as sh:
 		for i in data:
 			sh.write(i)
 			sh.write("\n")
-	with open("names.txt", "w+") as main_catalog:
+	with open("super-telephone-book\\names.txt", "w+") as main_catalog:
 		main_catalog.write("\n")
 		main_catalog.write('\n')
 		main_catalog.write(basic_info)
@@ -120,7 +121,7 @@ def option_one():
 
 def find_contact():
 	# opens file in read mode
-	fh = open("names.txt", "r")
+	fh = open("super-telephone-book\\names.txt", "r")
 	# requests input from the user
 	word = input("Input the contact you wish to find!\n >")
 	s = " "
@@ -145,13 +146,20 @@ def delete_contact():
 		for line in dataa :							
 			# condition for data to be deleted
 			if line.strip("\n") != delete_input : 
-								f.write(line)				
+								f.write(line)	
+	delete_path = 'super-telephone-book\\'+delete_input+'.txt'
+	if path.exists(delete_path):
+		remove(delete_path)
+	else:
+		print("The file does not exist")
+			
 					
 				
 
 
 def if_four():
 	# opens txt file in read mode
+
 	fo = open("super-telephone-book\\names.txt", 'r')
 	#reads the txt
 	contact_list=fo.read()
@@ -160,7 +168,23 @@ def if_four():
 	print('\n')
 
 
+def edit_contact():
+	if_four()
+	while True:
+		edit_input = input("Input the contact that you want to edit: ")
+		global edit_input_path
+		edit_input_path = 'super-telephone-book\\'+edit_input+'.txt'
+		if path.exists(edit_input_path):
+			notepad_run()
+			break
+		else:
+			print("Please input a valid contact")
+			continue
 
+
+	
+
+	
 
 def menu():
 	colorama.init()
@@ -168,45 +192,53 @@ def menu():
 	print(Fore.MAGENTA + Style.DIM + "(02)" + Style.RESET_ALL + "Search for contact")
 	print(Fore.MAGENTA + Style.DIM + "(03)" + Style.RESET_ALL + "Delete contact")
 	print(Fore.MAGENTA + Style.DIM + "(04)" + Style.RESET_ALL + "Contact List")
+	print(Fore.MAGENTA + Style.DIM + "(05)" + Style.RESET_ALL + "Edit Contact")
 	print(Fore.MAGENTA + Style.DIM + "(99)" + Style.RESET_ALL + "Exit")
 
 
+def cleanup():
+	clear()
+	logo()
 def main_main_process():
 	menu()
 	while True:
 		answer = int(input("Enter your option:"))
 		if answer == 1:
-			clear()
-			logo()
+			cleanup()
 			option_one()
 			menu()			
 		elif answer == 2:
-			clear()
-			logo()
+			cleanup()
 			find_contact()
 			menu()
 		elif answer == 3:
-				clear()
-				logo()
+				cleanup()
 				if_four()
 				global delete_input
 				delete_input = input("Input the contact from the ones above that you want to be deleted!\n Note. Copy and paste exactly the contact otherwise it wont work :D\n >")
 				delete_contact()
 				menu()	
-		elif answer == 4:			
+		elif answer == 4:
+				cleanup()			
 				if_four()
 				menu()	
+		elif answer == 5:
+			cleanup()
+			edit_contact()
 		else:
-			clear()
-			logo()
+			cleanup()
 			print("Input a number given in the options:")
 			menu()
 		if answer == 99:
-			clear()
-			logo()
+			cleanup()
 			print("Thanks for using this program.")
 			time.sleep(2)
 			quit()		
+
+def notepad_run():
+	subprocess.run(["notepad", edit_input_path])
+
+
 
 logo()
 welcome_text()
